a16605852b250a0db82f8e8ff3a34564074acc7c
vdElyn <vdelyn@protonmail.com>
2019-12-19 00:07:52 +0100

    Distance D1 + Stockage + Affichage + Détails des fcts

--- distance.c
+++ distance.c
@@ -1,6 +1,7 @@
 #include "distance.h"
 
-int compD1(int cv, int cw) {
+int charCompare(int cv, int cw) {
+	// Comparateur de caractères selon la matrice de distances (Pour D1)
 	if ( (cv == 'A' && cw == 'C') || (cv == 'C' && cw == 'A') || (cv == 'A' && cw == 'T') || (cv == 'T' && cw == 'A') 
 	|| (cv == 'C' && cw == 'G') || (cv == 'G' && cw == 'T') || (cv == 'T' && cw == 'G') ) {
 		return 2;
@@ -11,62 +12,78 @@ int compD1(int cv, int cw) {
 	return 0;
 }
 
-
-float distanceD1(int v, int w) {
-	float dist = 0.0;
-	int vl = compterCarac(v);
-	int wl = compterCarac(w);
-
-	FILE *fv = ouvrirFic(v);
-	FILE *fw = ouvrirFic(w);
-	
-	int cv, cw; //on va fgetc dans la seq v et w et les comparer
-	
-	if (vl > wl) {
-		for (int i = 0; i < wl; i++) //vl plus grand
-		{
-			cv = fgetc(fv);
-			cw = fgetc(fw);
-			
-			dist += compD1(cv, cw);
-		}
-		
-		for (int i = 0; i < (vl-wl); i++)
-			dist += 1.5;
-	}
-	else {
-		for (int i = 0; i < vl; i++) //wl plus grand
-		{
-			cv = fgetc(fv);
-			cw = fgetc(fw);
-			
-			dist += compD1(cv, cw);
-		}
-		
-		for (int i = 0; i < (wl-vl); i++)
-			dist += 1.5;
-	}
-	
-	
-	fclose(fv);
-	fclose(fw);
-	return dist;
-}
-
-
-float distanceD1v2(int v, int w) {
+Distance distanceD1(int v, int w, char** argv) {
+	// Structure de donnée Distance : 2 Séquences, 2 int associés, un float pour la distance
 	Distance D1;
-	//D1 = malloc(sizeof(Distance));
-	D1.V = initSeq(v);
-	D1.W = initSeq(w);
-		
+	// On initialise les 2 champs des 2 séquences (Seq & taille)
+	D1.V = initSeq(v, argv);
+	D1.W = initSeq(w, argv);
+	// Et on associe les numéros de séquences utlisés à la structure 
+	D1.v = v;
+	D1.w = w;
+	// Initialisation du float distance à 0
+	D1.dist = 0.0;
+	// On effectue les comparaisons en allant jusqu'à la fin de la plus longue chaîne ADN
 	for (int i = 0; i < D1.V.l || i < D1.W.l; i++) 
 	{
-		D1.dist += compD1(D1.V.sequence[i], D1.W.sequence[i]);
+		D1.dist += charCompare(D1.V.sequence[i], D1.W.sequence[i]);
 	}
-	
+	// Puis on ajoute à la distance les "blancs" du nombre de la différence des deux chaînes
 	for (int i = 0; i < fabs(D1.V.l - D1.W.l); i++)
 		D1.dist += 1.5;
+	// C'est la structure entière qui est retournée
+	return D1;
+}
+
+Distance *StockDistances(char **argv) {
+	int k = 0;
+	// *All est un tableau de structures Distance qui stock toutes les distances & séquences associées
+	// 190 distances différentes, *All doit contenir 190 fois la taille de la structure
+	// Note : On pourrait faire un double tour de boucle avant l'allocation en incrémentant k pour la preuve des 190 distances
+	Distance *All = malloc(190*sizeof(Distance));
+	for (int v = 1; v <= 19; v++) {
+		// La première boucle va jusqu'à la 19ème séquence pour la comparer avec la 20ème
+		for (int w = v+1; w <= 20; w++) {
+		// La deuxième boucle s'initialise à v+1 pour ne pas reprendre les distances déjà calculées (1_2 puis 2_1)
+			All[k] = distanceD1(v, w, argv);
+			// On incrémente k pour passer à la case suivante du tableau (k atteint les 190 en fin de boucle)	
+			k++;
+		}
+	}
 	
-	return D1.dist;
+	return All; // Rappel : All = tableau de 190 structures Distance
+}
+
+void afficherDistance(Distance D){
+	// Pour une structure Distance donnée, la fonction affiche les 2 séquences + n° associés, et leur distance
+	printf("%d %s\t %d %s\t   D_%d_%d :\t%.1f\n", D.v, D.V.sequence, D.w, D.W.sequence, D.v, D.w, D.dist);
+}
+
+void afficheAll(Distance *All, char** argv) {
+	// On reprend les mêmes boucles que celles du stockage pour afficher chaque case du tableau All
+	int k = 0;
+	for (int v = 1; v <= 19; v++)
+	{
+		for (int w = v+1; w <= 20; w++)
+		{
+			afficherDistance(All[k]);
+			k++;
+			printf("\n");
+		}
+	}
+}
+
+void libereMemoire(Distance D) {
+	// Pour une structure Distance donnée, free de la mémoire allouée aux 2 séquences ADN dans leurs struct respectives
+	free(D.V.sequence);
+	free(D.W.sequence);
+}
+
+void libereAll(Distance *All) {
+	// Pour libérer le tableau All, on free ses 190 cases puis son adresse propre
+	for (int k = 0; k < 190; k++)
+	{
+		libereMemoire(All[k]);
+	}
+	free(All);
 }
--- distance.h
+++ distance.h
@@ -4,7 +4,7 @@
 #include <math.h>
 #include "sequence.h"
 
-/* Matrice de distance de Levensetein :
+/* Matrice de distances :
  * 		A	C	G	T	 -
  * 	A	0	2	1	2	1.5
  * 	C	2	0	2	1	1.5
@@ -13,19 +13,30 @@
  *	-      1.5     1.5     1.5     1.5
  */
  
-/*	Deux séquences :
- * 	v = v1.v2....v (lv)
- *  w = w1.w2....w (lw)
- */
- 
 struct distance {
-	// Stocke les distances entre les paires de séquences contenues dans un répertoire
+	// Comprend 2 Struct Séquences (ADN & taille) + n° de séquence associé, ainsi qu'un float pour la distance
 	Sequence V;
+	int v;
 	Sequence W;
+	int w;
 	float dist;
 };
 typedef struct distance Distance;
 
-int compD1(int cv, int cw);
-float distanceD1(int v, int w);
-float distanceD1v2(int v, int w);
+
+int charCompare(int cv, int cw);
+// Comparateur de caractères selon la matrice de distances (Pour D1)
+Distance distanceD1(int v, int w, char** argv);
+// Distance D1, retourtant une structure Distance, expliquée en détail dans le fichier .c
+
+Distance *StockDistances(char **argv);
+// Stockage de toutes les distances entre paires de séquences dans un tableau de structure Distance
+
+void afficherDistance(Distance D);
+void afficheAll(Distance *All, char** argv);
+// Fonctions pour l'affichage et la mise en forme de toutes les distances & séquences associées 
+
+void libereMemoire(Distance D);
+// Libère les 2 champs de séquences compris dans une structure Distance
+void libereAll(Distance *All);
+// Libération de mémoire de toutes les séquences dans le tableau de struct Distance
