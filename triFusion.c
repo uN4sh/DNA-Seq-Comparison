c13461ea0af0708340f0cc628385ce0665b23bfa
vdElyn <vdelyn@protonmail.com>
2019-12-19 14:36:51 +0100

    Partie 2 : Tableau All trié par distances

---
+++ triFusion.c
@@ -0,0 +1,78 @@
+#include "triFusion.h"
+
+void fusion(Distance *All, int g, int m, int d) {
+	// On a juste deux tableau, deb à mil et mil-1 à fin, qu'on va fusionner
+	int i,j,k;
+	Distance *tmpAll = malloc((d-g+1)*sizeof(Distance));
+	if(tmpAll == NULL) {
+		fprintf(stderr, "Erreur allocation tmpAll\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	i = g; j = m+1; k = 0;
+	// on va prendre la plus petite valeur des deux tableau et la mettre dans tmpAll
+	while(i < m+1 && j < d+1) {
+		if(All[i].dist < All[j].dist) {
+			// On compare juste les distances entre elles, mais pour les échange c'est la structure entiere
+			tmpAll[k] = All[i]; // Deux structures Distance 
+			i++;
+		}
+		else {
+			tmpAll[k] = All[j];
+			j++;
+		}
+		k++; 
+	}
+	// quand un des deux tableau est fini, on va juste ecrire le reste du tableau
+	// va faire un des deux while, ca depend lequel a fini en first
+	while(i < m+1) {
+		tmpAll[k] = All[i];
+		i++;
+		k++;
+	}
+	while(j<d+1) {
+		tmpAll[k] = All[j];
+		j++;
+		k++;
+	}
+	
+	
+	for (i = 0; i < d-g+1; i++) {
+		// on met tmpAll dans All // i+g pcq g pas forcément 0
+		All[i+g] = tmpAll[i];
+	}
+	// La mémoire de toutes les cases est vidée, reste juste à free le tableau lui même
+	free(tmpAll);
+}
+
+void triFusion(Distance *All, int g, int d) {
+	int m; // g, d et m indices du tableau
+	if(g < d) { // Tant que le tableau n'est pas vide
+		m = (g + d) / 2;
+		triFusion(All, g, m);
+		triFusion(All, m+1, d);
+		fusion(All, g, m, d);
+	}
+}
+
+void fileDistancesTriees(Distance *All) {
+	// Création d'un fichier des distances
+	FILE *fDist = fopen("stockDistancesTriées.txt", "w");
+	if(fDist == NULL) {
+		fprintf(stderr,"Echec lors de la création du fichier du stockage des distances triées\n");
+		exit(EXIT_FAILURE); 
+	}
+	int k = 0;
+	for (int v = 1; v <= 19; v++)
+	{
+		for (int w = v+1; w <= 20; w++)
+		{
+			fprintf(fDist, "%d %s\t %d %s\t   D_%d_%d :\t%.1f\n", All[k].v, All[k].V.sequence,
+				    All[k].w, All[k].W.sequence, All[k].v, All[k].w, All[k].dist);
+			k++;
+			fprintf(fDist, "\n");
+		}
+	}
+	fclose(fDist);
+	printf("Le fichier stockDistancesTriées.txt a été créé avec succès.\n");
+}
---
+++ triFusion.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "distance.h"
+
+void fusion(Distance *All, int g, int m, int d);
+void triFusion(Distance *All, int g, int d);
+
+void fileDistancesTriees(Distance *All);
